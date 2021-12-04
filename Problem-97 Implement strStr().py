ace4c92be30083caa0b32da147918e2cc9115f7d
Adhish Shrivastava <adhish70@gmail.com>
2021-12-04 15:23:53 -0500

    Initial Commit

---
+++ Problem-97 Implement strStr().py
@@ -0,0 +1,11 @@
+# 28. Implement strStr()
+# https://leetcode.com/problems/implement-strstr/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        pass
---
+++ Problem-98 Find All Anagrams in a String.py
@@ -0,0 +1,11 @@
+# 438. Find All Anagrams in a String
+# https://leetcode.com/problems/find-all-anagrams-in-a-string/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        pass
